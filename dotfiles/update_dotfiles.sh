1ce979bfdb4711357c5432cebd0f419a00de4177
Marc Zottner <marc@zottner.com>
2019-12-16 11:12:08 +0100

    Consolidated update script

--- dotfiles/update_dotfiles.sh
+++ dotfiles/update_dotfiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+pushd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null || exit
 
 #git pull origin master;
 
@@ -13,13 +13,15 @@ function doIt() {
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "${1}" == "--force" ] || [ "${1}" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -r  -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
 		doIt;
 	fi;
 fi;
 unset doIt;
+
+popd &>/dev/null || exit
